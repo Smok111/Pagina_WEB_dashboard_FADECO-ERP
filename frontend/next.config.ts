74d3e3cd3c51d50c05bdec94a2a5aefd9205b8ac
Luis Anthony Herrera Zarate <N00282873@UPN.PE>
2026-06-29 20:05:09 -0500

    fix: actualizar next.config.ts para produccion

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -9,7 +9,9 @@ const nextConfig: NextConfig = {
       },
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:4000/api/:path*',
+        destination: process.env.NEXT_PUBLIC_API_URL 
+          ? `${process.env.NEXT_PUBLIC_API_URL}/:path*` 
+          : 'http://127.0.0.1:4000/api/:path*',
       },
     ];
   },
